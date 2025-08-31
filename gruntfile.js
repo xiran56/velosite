fd020c7116b13eaa53b393746b960184517b12c6
xiran56 <xiranrtx@gmail.com>
2025-08-31 12:12:54 +0500

    Basic setup

---
+++ gruntfile.js
@@ -0,0 +1,103 @@
+
+module.exports = function(grunt) {
+    const JST_PATH = 'build/templates.js'
+    const TEMPLATES_DATA_PATH = 'src/templates_data.json'
+
+    var processhtml_files = {}
+    var htmlmin_files = {}
+    var fs = require('fs')
+    var files = fs.readdirSync('src')
+
+    files.forEach(function (file, i) {
+        console.log(file)
+        if (!file.endsWith('.handlebars'))
+            return
+
+        const page = file.substring(0, file.length - 11)
+        const toBeMinifiedName =  'dist/' + page + '.full.html'
+
+        processhtml_files[toBeMinifiedName] = ['build/' + page + '.html']
+        htmlmin_files['dist/' + page + '.html'] = toBeMinifiedName
+    })
+
+    // Project configuration.
+    grunt.initConfig({
+      pkg: grunt.file.readJSON('package.JSON'),  
+      handlebars: {
+          compile: {
+              options: {
+                  namespace: 'JST',
+                  commonjs: true
+              },
+              files: {
+                  'build/templates.js': ['src/*.handlebars']
+              },
+          }
+        },
+
+        cssmin: {
+             minify: {
+                 options: {
+                     banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                 },
+                 cwd: './src',
+                 src: ['*.css'],
+                 dest: 'build',
+                 ext: '.css',
+                 expand: true
+             }
+         },
+         uglify: {
+             options: {
+                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+             },
+             build: {
+                 src: ['src/*.js'],
+                 dest: 'build/',
+                 ext: '.js'
+             }
+         },
+         processhtml: {
+             dist: {
+                 options: {
+                     process: true,
+                     data: {
+                         title: 'My app',
+                         message: 'This is production distribution'
+                     }
+                 },
+                 files: processhtml_files 
+             }
+         },
+         htmlmin: {
+             dist: {
+                 options: {
+                     removeComments: true,
+                     collapseWhitespace: true
+                 },
+                 files: htmlmin_files
+             }
+         },
+
+         clean: ['dist*//*.full.*', 'build/*']
+
+    });
+
+    var jstInstantiate = require('./build_scripts/jst_instantiation')
+
+    grunt.loadNpmTasks('grunt-contrib-handlebars');
+
+    grunt.registerTask('jst_instantiation', function() {
+        grunt.task.requires('handlebars')
+        jstInstantiate(JST_PATH, TEMPLATES_DATA_PATH, 'build')
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-processhtml');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask('default', ['handlebars', 'jst_instantiation', 'cssmin','uglify', 'processhtml', 'htmlmin', 'clean']);
+    
+};
